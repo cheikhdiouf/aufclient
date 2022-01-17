a3bada6a519d5f1ad42c3b8e60aec82f37a4fada
cheikh <dioufcheikhtt@gmail.com>
2022-01-17 15:36:54 +0000

    hebergement du projet

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service'
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[];
+
+  constructor(private employeeService: EmployeeService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.getEmployee()
+  }
+  private getEmployee() {
+    this.employeeService.getEmployeeList().subscribe(data => {
+      this.employees = data;
+    });
+  }
+  detailEmployee(id:number){this.router.navigate(['detail',id])}
+  updateEmployee(id:number)
+  {this.router.navigate(['update',id])
+}
+deleteEmployee(id:number)
+{
+  this.employeeService.deleteEmployee(id).subscribe(data=>{
+    console.log(data);
+    this.getEmployee()
+}
+)
+}
+}
